2d5ed1c8c2303763af7fd6fd58e5fb7e07115b73
Ekaterina Ch <enc1@tpu.ru>
2017-08-15 21:43:40 +0700

    sliders funcs

--- frontend/sliders/sliders.js
+++ frontend/sliders/sliders.js
@@ -1,10 +1,10 @@
-function sliderLine(slider) {
-    let sl = $(slider);
+function sliderLine() {
+    let sl = $('.sliders__second_range2');
     sl.css('background', 'linear-gradient(to right, #4eb7a8 0%, #4eb7a8 ' + sl.val() + '%, #e5e5e5 ' + sl.val() + '%, #e5e5e5 100%)');
 }
 
-function sliderOutput(slider) {
-    el = $(slider);
+function sliderOutput() {
+    el = $('.sliders__first_range1');
     width = el.width();
     newPoint = (el.val() - el.attr("min")) / (el.attr("max") - el.attr("min"));
     offset = -1.3;
@@ -19,14 +19,14 @@ function sliderOutput(slider) {
 
 $( document ).ready(function() {
 
-    sliderOutput(".sliders__first_range1");
+    sliderOutput();
     $(".sliders__first_range1").on('input', function() {
-        sliderOutput(this);
+        sliderOutput();
     });
 
-    sliderLine(".sliders__second_range2");
+    sliderLine();
     $(".sliders__second_range2").on('input', function () {
-        sliderLine(this);
+        sliderLine();
     });
 });
 
