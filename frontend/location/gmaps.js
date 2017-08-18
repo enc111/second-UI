3ea3e81a021fa95dcee4a23fc7241104559416b3
Ekaterina Ch <enc1@tpu.ru>
2017-08-18 20:32:46 +0700

    border radius for news & events and maps, begin of calendar

--- frontend/location/gmaps.js
+++ frontend/location/gmaps.js
@@ -3,12 +3,14 @@ import * as gmap3 from 'gmap3'
 
 $( document ).ready(function() {
     $('#map').gmap3({
+
         address: "San Francisco, 1259 California Str.",
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     })
-    .marker({
+   .marker({
         position: [37.7912, -122.4151],
         icon: require('./marker.png')
-      });
+    });
+
 });
